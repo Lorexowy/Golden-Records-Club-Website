725523440bcf0b86aaaf9556feb6cf167f8eabe6
MatushM <slash97vp.pl@gmail.com>
2022-09-13 22:21:33 +0200

    OnScroll animations added

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+
+// Phone Menu navbar script 
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navLinks = document.getElementsByClassName('nav-links')[0];
 
 toggleButton.addEventListener('click', () => {
   navLinks.classList.toggle('active')
 })
+
+
+// Fade-in animation script
+
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px -250px 0px"
+};
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
